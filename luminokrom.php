b1ea560b31d7003afce3a92bf78c0b727bddf50b
moulayeHaidara10 <moulayehaidara0@gmail.com>
2022-07-11 22:42:57 +0000

    modifications de la page luminokrom

--- luminokrom.php
+++ luminokrom.php
@@ -1,6 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+================================================== -->
+  <meta charset="utf-8">
+  <title>Capital Process - Luminokrom</title>
+
+  <!-- Mobile Specific Metas
+================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Construction Html5 Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+
+  <!-- Favicon
+================================================== -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+  <!-- CSS
+================================================== -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+  <!-- Animation -->
+  <link rel="stylesheet" href="plugins/animate-css/animate.css">
+  <!-- slick Carousel -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick/slick-theme.css">
+  <!-- Colorbox -->
+  <link rel="stylesheet" href="plugins/colorbox/colorbox.css">
+  <!-- Template styles-->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  <div class="body-inner">
 <?php require 'feature/header.php'; ?>
+<div id="banner-area" class="banner-area" style="background-image:url(images/banner/banner1.jpg)" style="margin-top: 90px">
+  <div class="banner-text">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+              <div class="banner-heading">
+                <h1 class="banner-title">Luminokrom</h1>
+               
+              </div>
+          </div><!-- Col end -->
+        </div><!-- Row end -->
+    </div><!-- Container end -->
+  </div><!-- Banner text end -->
+</div>
+<section id="main-container" class="main-container pb-2">
+  <div class="container">
+    <div class="row">
+    <p>La technologie LuminoKrom® est brevetée et fonctionne sans électricité ni émission de CO2.</p>
+<p><strong>Les peintures photoluminescentes LuminoKrom® sont les plus performantes du marché</strong> (<a href="https://www.luminokrom.com/luminescence-norme-internationale/">durées de luminance certifiées,</a> norme CE ISO17398).</p>
+				<div class="clearfix"></div>
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/luminokrom_vert-amande.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon1.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="#">LuminoKrom® Vert amande	</a></h3>
+                  <p>Peinture urbaine photoluminescente pour mobilités douces</p>
+                  <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service1 end -->
+      </div><!-- Col 1 end -->
 
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/luminokrom_vert-amande.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon2.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="service-single.html">LuminoKrom® Vert amande Métal</a></h3>
+                  <p>Peinture photoluminescente pour supports métalliques</p>
+                  <a class="learn-more d-inline-block" href="#" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service2 end -->
+      </div><!-- Col 2 end -->
 
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/luminokrom_blanc.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon3.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="service-single.html">LuminoKrom® Blanc</a></h3>
+                  <p>Peinture urbaine photoluminescente pour zones circulées et non circulées</p>
+                  <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service3 end -->
+      </div><!-- Col 3 end -->
+
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/luminokrom_blanc.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon4.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="service-single.html">LuminoKrom® Blanc réfléchissant</a></h3>
+                  <p>Peinture routière photoluminescente pour zones circulées et non circulées</p>
+                  <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service1 end -->
+      </div><!-- Col 4 end -->
+
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/enduit-a-froid.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon5.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="service-single.html">LuminoKrom® Enduit à froid</a></h3>
+                  <p>Enduit à froid blanc urbain photoluminescent pour zones circulées</p>
+                  <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service2 end -->
+      </div><!-- Col 5 end -->
+
+      <div class="col-lg-4 col-md-6 mb-5">
+        <div class="ts-service-box">
+            <div class="ts-service-image-wrapper">
+              <img loading="lazy" class="w-100" src="images/luminokrom_jaune.jpeg" alt="service-image">
+            </div>
+            <div class="d-flex">
+              <div class="ts-service-box-img">
+                  <img loading="lazy" src="images/icon-image/service-icon6.png" alt="service-icon">
+              </div>
+              <div class="ts-service-info">
+                  <h3 class="service-box-title"><a href="service-single.html">LuminoKrom® Jaune</a></h3>
+                  <p>Peinture routière photoluminescente urbaine temporaire</p>
+                  <a class="learn-more d-inline-block" href="service-single.html" aria-label="service-details"><i class="fa fa-caret-right"></i> Lire la suite</a>
+              </div>
+            </div>
+        </div><!-- Service3 end -->
+      </div><!-- Col 6 end -->
+
+    </div><!-- Main row end -->
+  </div><!-- Conatiner end -->
+</section>
 
 
 
